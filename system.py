b168588f7f97545e017324d9728f1ac9792a4a05
William Ellis <wellis3@sheffield.ac.uk>
2024-12-20 16:40:07 +0000

    FINAL CHANGES

--- system.py
+++ system.py
@@ -1,25 +1,24 @@
-import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.base import BaseEstimator, ClassifierMixin
 
 def image_to_reduced_feature(images, split='train'):
 
-    principle_components = 70
+    # Number of priciple components wanted
+    principle_components = 80
 
+    #Covarience Matrix is calculated
     covx = np.cov(images, rowvar=0)
     N = covx.shape[0]
+    # Eigenvectors and values are calculated
     w, v = np.linalg.eigh(covx)
+    # Eigenvectors and values and sorted in descending order of varience and truncated at the number of priciple components
     w = w[-principle_components:]
     v = v[:, -principle_components:]
     v = np.fliplr(v)
-    v.shape
 
-    N = 70
+    # Mean of the data is found which is used to normalise the data set, mean of 0 and range -1 - 1
     mean_train = np.mean(images, axis=0)
 
-    # Create a figure for displaying the images
-    plt.figure(figsize=(10, 10))
-
+    # Images are reconstruced from the eigenvectors only keeping the most important information
     reconstructed = (
         np.dot(
             np.dot(images - mean_train, v[:, 0: N - 1]),
@@ -27,7 +26,7 @@ def image_to_reduced_feature(images, split='train'):
         )
         + mean_train
     )
-
+    # The smaller reconstructed images are retruned, reducing the time taken to run the program
     return reconstructed
 
 
@@ -36,8 +35,8 @@ def training_model(train_features, train_labels):
     model.fit(train_features, train_labels)
     return model
 
-class KNeighboursClassifier(BaseEstimator, ClassifierMixin):
-    def __init__(self, n_neighbours=6):
+class KNeighboursClassifier():
+    def __init__(self, n_neighbours=4):
         self.n_neighbours = n_neighbours
         self.train_data = None
         self.train_labels = None
@@ -48,13 +47,13 @@ class KNeighboursClassifier(BaseEstimator, ClassifierMixin):
         return self
 
     def predict(self, test_data):
-        # Compute cosine similarity as before
+        # Calculate cosine similarity
         x = np.dot(test_data, self.train_data.T)
         modtest = np.sqrt(np.sum(test_data * test_data, axis=1))
         modtrain = np.sqrt(np.sum(self.train_data * self.train_data, axis=1))
         dist = x / np.outer(modtest, modtrain)  # Cosine similarity matrix
 
-        # Since we're using cosine similarity, convert it to a distance measure (1 - similarity)
+        # Convert to a distance measure (1 - similarity)
         distances = 1 - dist
 
         # Find indices of the nearest neighbors
@@ -64,7 +63,7 @@ class KNeighboursClassifier(BaseEstimator, ClassifierMixin):
         nearest_labels = self.train_labels[nearest_indices]
         nearest_distances = np.take_along_axis(distances, nearest_indices, axis=1)
 
-        # Compute weights as inverse of distances (adding a small epsilon to avoid division by zero)
+        # Compute weights as inverse of distances (adding tiny number (does not effect reuslts) in order to avoid devision by zero errors)
         weights = 1 / (nearest_distances + 1e-8)
 
         # Perform weighted voting
@@ -76,6 +75,7 @@ class KNeighboursClassifier(BaseEstimator, ClassifierMixin):
 
         # Predict the label with the highest weighted vote
         predictions = np.argmax(weighted_votes, axis=1)
-
+        
+        # Return the predictions
         return predictions
 
